8bb988120a23d32c9e87ba22a5359e1f0b7dcad2
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-08-21 15:24:47 +0530

    addd: progress bar

--- nextjs-app/src/pages/_app.tsx
+++ nextjs-app/src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import "../styles/App.css";
 import "../styles/index.css";
+import NextNProgress from "nextjs-progressbar";
 import type { AppType } from "next/dist/shared/lib/utils";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
