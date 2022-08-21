7dbd4af50f687f31b23bfec687989d72782d933e
Kishan Joshi <robokishan.blogspot@gmail.com>
2022-08-21 15:14:06 +0530

    fix: build issue

--- nextjs-app/src/Components/App.tsx
+++ nextjs-app/src/Components/App.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-target-blank */
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import LongText from "./LongText";
 import MockData from "./MockData";
