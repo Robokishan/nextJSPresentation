bb39d9c55218dde3e9dcaa1aa4d0fa51a18113c2
Kishan Joshi <kishan.joshi@SF-CPU-218.local>
2022-08-22 14:48:07 +0530

    add: loader

--- nextjs-app/src/Components/MockData/index.tsx
+++ nextjs-app/src/Components/MockData/index.tsx
@@ -7,6 +7,7 @@ interface MockDataProps {
 export default function index({ data }: MockDataProps): ReactElement {
   return (
     <div>
+      {Object.keys(data).length === 0 && <><strong>Data is loading please wait...</strong></>}
       {Object.keys(data).map((key) => (
         <div key={key}>
           <strong>{key}: </strong>
--- react-app/src/MockData/index.tsx
+++ react-app/src/MockData/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 export default function index() {
   const [data, setData] = useState("");
   useEffect(() => {
+    console.log("hello world");
     fetch("https://random-data-api.com/api/address/random_address").then(
       (response) => response.json().then((data) => setData(data))
     );
@@ -10,12 +11,18 @@ export default function index() {
 
   return (
     <div>
-      {Object.keys(data).map((key) => (
-        <div key={key}>
-          <strong>{key}: </strong>
-          <span>{data[key]}</span>
-        </div>
-      ))}
+      {Object.keys(data).length === 0 ? (
+        <>
+          <strong>Data is loading please wait...</strong>
+        </>
+      ) : (
+        Object.keys(data).map((key) => (
+          <div key={key}>
+            <strong>{key}: </strong>
+            <span>{data[key]}</span>
+          </div>
+        ))
+      )}
     </div>
   );
 }
